5d06bfb50a74d3106464d13b9f0a2f93934e8ca6
Mitchopp <100094055+Mitchopp@users.noreply.github.com>
2024-05-24 21:59:02 +0100

    Update start.py

--- start.py
+++ start.py
@@ -4,11 +4,23 @@ import sys
 from sprites import *
 from config import *
 
-# Initialize Pygame
+# initalize pygame
 class Game:
     def __innit__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode(WIN_WIDTH, WIN_HEIGHT)
+        self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.font = pygame.font.Font("arial", 32)
+        self.running = True
+
+    def new(self):
+        # add a new game start
+        self.playing = True
+
+        self.all_sprites = pygame.sprite.LayeredUpdates()
+        self.blocks = pygame.sprite.LayeredUpdates()
+        self.enemies = pygame.sprite.LayeredUpdates()
+        self.attacks = pygame.sprite.LayeredUpdates()
 
 # Set the width and height of the screen (width, height)
 SCREEN_SIZE = (800, 600)
